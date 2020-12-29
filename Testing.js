ee512215fb7473a7201d8339727f049faff42235
LinuxSecAdmin <69774420+LinuxSecAdmin@users.noreply.github.com>
2020-12-28 21:24:48 -0800

    Comment

--- Testing.js
+++ Testing.js
@@ -1,7 +1,10 @@
 var sayName = "Sam";
 var num = 23;
+
+//Writes to Webpage
 document.write("Hello there, " + sayName + ".");
 document.write('\nI am ' + num + ' years old.');
 
+//Writes to Web Console
 console.log("Hello there, " + sayName + ".");
 console.log('\nI am ' + num + ' years old.');
